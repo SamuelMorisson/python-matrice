e13e2ee701b89c56a6588f0ed798f5f37d8ef4c3
SamuelMorisson <morissonsamuel@gmail.com>
2024-12-09 16:57:47 +0100

    ajout TP Matrice sur github

---
+++ Matrice.py
@@ -0,0 +1,59 @@
+class Matrice:
+    def __init__(self, taille_matrices : tuple):
+        self.__taille_matrices = taille_matrices
+
+    def get_matrices(self):
+        return self.__taille_matrices
+    
+    def afficher_matrices(self):
+        print("\nAFFICHAGE")
+        for matrice in self.__taille_matrices:
+            print("------------------------------")
+            a = []
+            for i in range(matrice):
+                b = []
+                for j in range(matrice):
+                    b.append(0)
+                a.append(b)
+
+            for i in range(len(a)):
+                print(a[i])
+
+
+    def somme_matrice(self):
+        print("\nSOMME")
+        somme = 0
+        for matrice in self.__taille_matrices:
+            somme += matrice
+        print("------------------------------")
+        a = []
+        for i in range(somme):
+            b = []
+            for j in range(somme):
+                b.append(0)
+            a.append(b)
+        for i in range(len(a)):
+            print(a[i])
+
+
+    def multiplication_matrice(self):
+        print("\nMULTIPLE")
+        multiple = 1
+        for matrice in self.__taille_matrices:
+            multiple *= matrice
+        print("------------------------------")
+        a = []
+        for i in range(multiple):
+            b = []
+            for j in range(multiple):
+                b.append(0)
+            a.append(b)
+        for i in range(len(a)):
+            print(a[i])
+
+listeTaille : tuple = (2, 4)
+test = Matrice(listeTaille)
+test.afficher_matrices()
+test.somme_matrice()
+test.multiplication_matrice()
+
